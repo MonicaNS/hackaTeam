ef130b6818823df9fe94c6a182ed2988185d0d37
preranasunilkumar <preranasunil7@gmail.com>
2020-02-15 10:15:12 -0800

    linked buttons on the sign up and login pages

--- app/src/main/java/com/example/hackateam/login.java
+++ app/src/main/java/com/example/hackateam/login.java
@@ -2,13 +2,29 @@ package com.example.hackateam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                backSignUp();
+            }
+        });
+    }
+    public void backSignUp(){
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
     }
 }
